4b081d95814254dd150a6c1e0ec38e716eaf07b7
simonnemcic <simonnemci35@gmail.com>
2025-03-18 23:39:35 +0100

    feat: env for storing tokens

--- baselinker_api.py
+++ baselinker_api.py
@@ -1,49 +1,17 @@
 import requests
 import json
 from tabulate import tabulate
-import gspread
+import dotenv
+
+dotenv.load_dotenv()
 
 
 API_URL = "https://api.baselinker.com/connector.php"
-TOKEN = "6000739-6000727-7C9O730TUE77SJ795CG9UP4H6HWHAGH3X2MY2CDVA0V41T7U39EPYSFUV52PHLWB"
+TOKEN = dotenv.dotenv_values(".env")["TOKEN"]
 INVENTORY_ID = 833
 TARGET_PRODUCT_ID = "12064368"
 EXTRA_FIELD_1_ID = "extra_field_483"
 EXTRA_FIELD_2_ID = "extra_field_484"
-
-
-# Google Sheets API setup
-credentials = {
-    "installed": {
-        "client_id": "12345678901234567890abcdefghijklmn.apps.googleusercontent.com",
-        "project_id": "my-project1234",
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_secret": "MySecRet....",
-        "redirect_uris": ["http://localhost"]
-    }
-}
-authorized_user = {
-    "refresh_token": "8//ThisALONGTOkEn....",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "client_id": "12345678901234567890abcdefghijklmn.apps.googleusercontent.com",
-    "client_secret": "MySecRet....",
-    "scopes": [
-        "https://www.googleapis.com/auth/spreadsheets",
-        "https://www.googleapis.com/auth/drive"
-    ],
-    "expiry": "1070-01-01T00:00:00.000001Z"
-}
-
-gc, authorized_user = gspread.oauth_from_dict(credentials, authorized_user)
-
-# Access the spreadsheet
-sh = gc.open("Example spreadsheet")
-
-# Example: Read a cell value
-cell_value = sh.sheet1.get('A1')
-print(f"Value in A1: {cell_value}")
 
 
 def make_request(method, parameters=None):
@@ -87,3 +55,4 @@ def update_product_extra_field(inventory_id, product_id, field_id, value):
         }
     }
     return make_request("addInventoryProduct", parameters)
+
